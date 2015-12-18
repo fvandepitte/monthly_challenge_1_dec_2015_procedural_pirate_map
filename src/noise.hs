2c48910124f176a8f68ca11b3ae60aed62a4b2cf
fvandepitte <frederiek.vandepitte@gmail.com>
2015-12-18 15:51:07 +0100

    added working noise

--- src/noise.hs
+++ src/noise.hs
@@ -1,34 +1,55 @@
 module Noise where
 
 import System.Random
-import Data.List (last)
 import Numeric.Noise 
 import Numeric.Noise.Perlin
 
+type NoiseGen = Int -> Int -> Int -> [Double]
 
-data Universe = Universe Int Int [Bool]
+data Universe = Universe Int Int [Double]
 
 instance Show Universe where
-    show (Universe _ cols cells) = unlines $ toRows cols $ map toChar cells
-        where toRows _    []     = []
-              toRows cls cells'  = take cls cells' : (toRows cls $ drop cls cells')
-              toChar True  = '*'
-              toChar False = ' '
+    show (Universe _ cols cells)   = unlines $ toRows cols $ map toChar cells
+        where toChar x | x > 0     = '*'
+                       | otherwise = ' '
 
-createUniverse :: RandomGen g => g -> Int -> Int -> Double -> Universe
-createUniverse gen rows cols r =
-    let grid   = [ (x, y, 0) | x <- [0 .. fromIntegral cols], y <- [0 .. fromIntegral rows]]
-        sphere = createSphere rows cols r grid
-     in Universe rows cols $ map (>0) $ zipWith (+) sphere $ zipWith (*) sphere $ map (noiseValue (perlinGen (fst (random gen)))) $ grid
+toRows :: Int -> [a] -> [[a]]
+toRows _  [] = []
+toRows cols xs =
+  let s = splitAt cols xs
+   in fst s : toRows cols (snd s)
 
-createSphere :: Int -> Int -> Double -> [Point] -> [Double]
-createSphere rows cols r grid = 
+createUniverse :: Int -> Int -> Int -> NoiseGen -> Universe
+createUniverse seed rows cols noiseGen = Universe rows cols $ noiseGen seed rows cols
+
+createPerlinData :: NoiseGen
+createPerlinData seed rows cols = map (noiseValue (perlinGen (fst (random (mkStdGen seed))))) $ createGrid rows cols
+
+createRidgedData :: NoiseGen
+createRidgedData seed rows cols = map (noiseValue (ridgedGen (fst (random (mkStdGen seed))))) $ createGrid rows cols
+
+createSphereData :: NoiseGen
+createSphereData r rows cols = createSphere (fromIntegral r) $ createGrid rows cols
+
+fuse :: Universe -> Universe -> Universe
+fuse u (Universe _ _ cells) = addLayer u cells
+
+addLayer :: Universe -> [Double] -> Universe
+addLayer (Universe rows cols cells) ld = Universe rows cols $ zipWith (average) cells ld
+
+average :: Double -> Double -> Double
+average a b = (a + b) / 2
+
+createGrid :: Int -> Int -> [Point]
+createGrid rows cols = [ (x, y, 0) | y <- [0 .. fromIntegral (rows - 1)], x <- [0 .. fromIntegral (cols - 1)]]
+
+createSphere :: Double -> [Point] -> [Double]
+createSphere r grid = 
     let centerPoint = getCenterPoint grid
      in map (createSphere' r . distanceBetween centerPoint) grid
 
 createSphere' :: Double -> Double -> Double
-createSphere' r d | d > r     = 0
-                  | otherwise = r - d
+createSphere' r d = (r - d) / r
 
 getCenterPoint :: [Point] -> Point
 getCenterPoint ps = 
@@ -40,7 +61,7 @@ halfWayPoint :: Point -> Point -> Point
 halfWayPoint (x1, y1, z1) (x2, y2, z2) = ((x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2)
 
 distanceBetween :: Point -> Point -> Double
-distanceBetween (x1, y1, z1) (x2, y2, z2) = sqrt ((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2) 
+distanceBetween (x1, y1, z1) (x2, y2, z2) = sqrt ((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2) 
 
 octaves :: Int
 octaves = 5
@@ -51,8 +72,17 @@ scale = 0.05
 persistance :: Double
 persistance = 0.5
 
+frequency :: Double
+frequency = 1
+
+lacunarity :: Double
+lacunarity  = 2
+
 perlinGen :: Int -> Perlin
 perlinGen s = perlin s octaves scale persistance
+
+ridgedGen :: Int -> Ridged
+ridgedGen s = ridged s octaves scale frequency lacunarity
 
 mapToString :: [Bool] -> String
 mapToString = map mapToString'
